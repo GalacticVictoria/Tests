d52f92825cf93b192f459b369a1150d5a2c631ad
Lindsay Victoria Abbott <IzzieAugustProductions@gmail.com>
2026-07-17 14:55:02 -0400

    Created MeshesGeometry script for storing custom mesh arrays

---
+++ MeshesGeometry.ts
@@ -0,0 +1,18 @@
+export const crystalMesh = {
+  verts: new Float32Array([
+    -0.168419, 0.000000, 0.467116,
+    -0.168419, 0.867183, -0.000000,
+    // ...
+  ]),
+
+  uvs: new Float32Array([
+    0.375000, 1.000000,
+    // ...
+  ]),
+
+  triangles: new Int32Array([
+    0,1,2,
+    0,2,3,
+    // ...
+  ]),
+};
