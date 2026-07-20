c0de0185ac8c100605d82287fffeb9bc986f35d9
Lindsay Victoria Abbott <IzzieAugustProductions@gmail.com>
2026-07-20 13:22:53 -0400

    fix: update ambient light energy and procedural sky gradient curves in SkyDome

--- SkyDome.ts
+++ SkyDome.ts
@@ -7,17 +7,17 @@ registerStart(start);
 function start() {
   // Ambient light tint
   SkyDome.ambientLight.baseColor.set(new Color(1.0, 1.0, 1.0));
-  SkyDome.ambientLight.energy.set(.001);
+  SkyDome.ambientLight.energy.set(.01);
   SkyDome.ambientLight.skyColorContribution.set(0.0);
 
   // Procedural sky gradient
   SkyDome.skyMaterial.setProceduralSkyMaterial(
     new Color(1.0, 0.0, 0.0),  // top color
     new Color(1.0, 0.78, 0.0),  // top horizon color
-    .01,                            // top curve
+    .09,                            // top curve
     new Color(1.0, 0.0, 0.0),     // bottom color
     new Color(1.0, 0.78, 0.0),   // bottom horizon color
-    .01                             // bottom curve
+    .09                             // bottom curve
   );
 
 }
